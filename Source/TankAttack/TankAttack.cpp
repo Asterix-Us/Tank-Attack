a239965da9c37bb4f9bf6b3df8b84c06a172829e
Vasile Bogdan-Catalin <vasilebogdan235@gmail.com>
2023-11-15 13:27:45 +0200

    Added initial project files.

---
+++ Source/TankAttack/TankAttack.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TankAttack.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankAttack, "TankAttack" );
+
+DEFINE_LOG_CATEGORY(LogTankAttack)
+ 
---
+++ Source/TankAttack/TankAttackGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankAttackGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATankAttackGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATankAttackGameMode();
+};
+
+
+
